659c5b164ce57216afb9303bdd1711e779bda95f
Juan Diaz <jfdiaza@unal.edu.co>
2023-08-16 23:42:45 -0400

    init app

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+Greeting.create(message: "Hello Friends!")
+Greeting.create(message: "Hello Students!")
+Greeting.create(message: "Hi Guys!")
+Greeting.create(message: "Hi Strangers!")
+Greeting.create(message: "Hello People!")
