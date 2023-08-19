3a60bb42af793da779a9fa391bf708412fd2982f
Juan Diaz <jfdiaza@unal.edu.co>
2023-08-19 04:54:40 -0400

    correct linters issues

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,6 @@
 class MessagesController < ApplicationController
-    def index
-      random_greeting = Greeting.order('RANDOM()').first
-      render json: { greeting: random_greeting&.message }
-    end
+  def index
+    random_greeting = Greeting.order('RANDOM()').first
+    render json: { greeting: random_greeting&.message }
   end
-  
+end
